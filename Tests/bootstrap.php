ebe3924c7da4d9611e9b73c57520d7dc5a880ced
Vic Metcalfe <vic@acm.org>
2016-12-31 18:20:56 -0500

    Improved testing bootstrap autoloader

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,9 +1,27 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 use Doctrine\Common\Annotations\AnnotationRegistry;
+
+(function () {
+    $path = explode(DIRECTORY_SEPARATOR, __DIR__);
+
+    $autoloadPath = function ($path) {
+        array_push($path, 'vendor');
+        array_push($path, 'autoload.php');
+        return implode(DIRECTORY_SEPARATOR, $path);
+    };
+
+    while (!empty($path) && !file_exists($autoloadPath($path))) {
+        array_pop($path);
+    }
+    require_once $autoloadPath($path);
+
+    array_push($path, 'app');
+//    array_push($path, 'AppKernel.php');
+    $_SERVER['KERNEL_DIR'] = implode(DIRECTORY_SEPARATOR, $path);
+//    require_once implode(DIRECTORY_SEPARATOR, $path);
+})();
 
 AnnotationRegistry::registerFile(__DIR__ . "/Fixtures/AnnotationA.php");
 AnnotationRegistry::registerFile(__DIR__ . "/Fixtures/AnnotationB.php");
